9bea2a81fbc9edad1a5fedcb4076c32a912ecc72
Felipe Rasinhas <feliperasinhas@vialink.com.br>
2013-05-21 10:23:52 -0300

    adding codeforces round-184 E problem

--- codeforces/div2-183/e.cpp
+++ codeforces/div2-183/e.cpp
@@ -11,20 +11,18 @@ int mod(int a) { return a > 0 ? a : -a; }
 
 int main()
 {
-    int n, k, m;
+    int n, k, m, v;
     int maxv=0;
     int a[6000];
     cin >> n >> k;
-    for(int i=0;i<n;i++)
-    {
-        cin >> a[i];
-        maxv = max(maxv,a[i]);
-    }
+    for(int i=0;i<n;i++) cin >> a[i];
     for(int i=0;i<n;i++) for(int j=i+1;j<n;j++) dif[mod(a[i]-a[j])]++;
     for(m=n-k;m<1000002;m++)
     {
         int kl = k;
-        if(dif[m] > k) continue;
+        v = 0;
+        for(int i=m;i<=1000002;i+=m) v += dif[i]; 
+        if(v > k*(k+1)/2) continue;
         for(int i=0;i<n;i++)
         {
             if(vis[a[i]%m] == m) kl--;
