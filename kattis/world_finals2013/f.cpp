f8a6f72cc4376147472203634c8409fa58b1dd70
Felipe Rasinhas <feliperasinhas@gmail.com>
2013-07-17 10:10:46 -0300

    WF2013-low power

---
+++ kattis/world_finals2013/f.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#define sz size()
+#define pb push_back
+
+using namespace std;
+
+int p[1001000];
+int dif[1001000];
+int n, k;
+int t;
+
+bool valid(int d)
+{
+    vector <int> s;
+    for(int i=0;i<t-1;i++)
+    {
+        if(dif[i] <= d) 
+        {
+            s.pb(i);
+            i++;
+        }
+        if(s.sz == n) break;
+    }
+    if(s.sz < n) return 0;
+    for(int i=s.sz-1;i>=0;i--)
+    {
+        int r = t - s[i];
+        int used = 2*(n-i-1)*k;
+        if(r-used < 2*k) return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    cin >> n >> k;
+    t = 2*n*k;
+    for(int i=0;i<t;i++) cin >> p[i];
+    sort(p,p+t);
+    for(int i=1;i<t;i++) dif[i-1] = p[i]-p[i-1];
+    int l = 0, h = p[2*n*k-1]-p[0];
+    int m;
+    int lv;
+    while(l <= h)
+    {
+        m = (l+h)/2;
+        if(valid(m)) 
+        {
+            lv = m;
+            h = m-1;
+        }
+        else l = m+1;
+    }
+    cout << lv << endl;
+    return 0;
+}
