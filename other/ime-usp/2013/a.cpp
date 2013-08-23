9e13e46dfe9f4fd7e0dd73564013932701799504
Felipe Rasinhas <feliperasinhas@gmail.com>
2013-08-23 20:55:43 -0300

    fixing ime-usp 2013 a

--- other/ime-usp/2013/a.cpp
+++ other/ime-usp/2013/a.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int a[111111];
-int ac[111111];
+long long ac[111111];
 
 int main()
 {
@@ -14,15 +14,15 @@ int main()
     while(scanf("%d", &n) != EOF)
     {
         for(int i=0;i<n;i++) cin >> a[i];
-        sort(a,a+n);
+        sort(a,a+n,greater<int>());
         ac[0] = a[0];
         for(int i=1;i<n;i++) ac[i] = ac[i-1]+a[i];
-        int pt = n-1;
-        int i;
+        long long pt = n-1;
+        long long i;
         for(i=0;i<n;i++)
         {
             while(a[pt] < i+1) pt--;
-            int s = ac[n-1]-ac[max(pt-1,i-1)] + (max(pt,i+1)-i-1)*(i+1);
+            long long s = ac[n-1]-ac[max(pt,i-1)] + (max(pt,i+1)-i)*(i+1);
             if(ac[i] > ((i+1)*i) + s) break;
         }
         if(i == n && !(ac[n-1]%2)) cout << "possivel" << endl;
