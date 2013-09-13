c67aa80e2269479ae2a5495212a87463ec14649f
Felipe Rasinhas <feliperasinhas@gmail.com>
2013-09-12 23:14:05 -0300

    changing the solution of a problem

--- codeforces/gym/training1-1/f.cpp
+++ codeforces/gym/training1-1/f.cpp
@@ -1,56 +1,52 @@
-#include <cstdio>
 #include <iostream>
-#include <algorithm>
+#include <cstdio>
 #include <string>
-#include <sstream>
-#define EPS 1e-12
+#include <cstring>
+
 using namespace std;
 
-double dp[111][111];
-string op[111];
-int n, T;
+//[pos][round]
+double dp[55][55];
+char c[55][5];
+int m;
 
-//REDO
-int conv(string s) {
-    stringstream ss(s);
-    int delta; ss>>delta;
-    return delta;
-}
-
-double play(int pos, int turn) {
-    if(pos >= n) return 1;
-    if(op[pos] == "L") {
-        if(turn == T) return 0;
-        return dp[pos][turn+1];
+void go(int x, int r, double p)
+{
+    x = min(x,m+1);
+    if(!strcmp(c[x],"L")) dp[x][r+1] += p;
+    else
+    {
+        int d;
+        sscanf(c[x],"%d", &d);
+        dp[min(m+1,x+d)][r] += p;
     }
-    int delta = conv(op[pos]);
-    return dp[pos+delta][turn];
 }
 
-int main() {
-    int tests;
-    cin>>tests;
-    while(tests--) {
-        cin>>n>>T;
-        n++;
-        for(int i=1;i<n;i++) cin>>op[i];
-        for(int i=0;i<n;i++) dp[i][T] = 0;
-        dp[n][T] = 1;
-        for(int i=T-1;i>=0;i--) {
-            dp[n][i] = 1;
-            for(int j=0;j<n;j++) {
-                dp[j][i] = 0;
-                dp[j][i] += play(j+1, i+1);
-                dp[j][i] += play(j+2, i+1);
-                dp[j][i] /= 2;
-                //cout<<j<<" "<<i<<" "<<dp[j][i]<<endl;
+int main()
+{
+    int t, r;
+    cin >> t;
+    while(t--)
+    {
+        double ret=0.0;
+        cin >> m >> r;
+        memset(dp,0,sizeof(dp));
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++) cin >> c[i+1];
+        c[m+1][0] = '0';
+        for(int i=0;i<=r;i++)
+        {
+            for(int j=0;j<m+1;j++)
+            {
+                go(j+1,i+1,dp[j][i]/2);
+                go(j+2,i+1,dp[j][i]/2);
             }
-            //cout<<endl;
+            ret += dp[m+1][i];
         }
-        double ans = dp[0][0];
-        if(ans < 0.5 - EPS) printf("Bet against. %.4lf\n", ans);
-        else if(ans > 0.5 + EPS) printf("Bet for. %.4lf\n", ans);
-        else printf("Push. %.4lf\n", ans);
+        if(ret > 0.5) printf("Bet for. ");
+        else if(ret < 0.5) printf("Bet against. ");
+        else printf("Push. ");
+        printf("%.4lf\n", ret);
     }
     return 0;
 }
